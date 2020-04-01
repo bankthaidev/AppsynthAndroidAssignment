e333d5e57c0e1f88dac75d21f868dd4038640be0
bankthaidev <bankthaidev@gmail.com>
2020-04-01 17:21:47 +0700

    Version 1.0 for Appsynth Android Assignment

--- app/src/main/java/co/th/appsynthandroidassignment/apiservice/ApiUtils.kt
+++ app/src/main/java/co/th/appsynthandroidassignment/apiservice/ApiUtils.kt
@@ -5,6 +5,6 @@ class ApiUtils {
     val BASE_URL = "https://testapi.io"
 
     fun getSOService(): SOService? {
-        return RetrofitClient.getClient(BASE_URL).create(SOService::class.java)
+        return RetrofitClient.getClient(BASE_URL)!!.create(SOService::class.java)
     }
 }
